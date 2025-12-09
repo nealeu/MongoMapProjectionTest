830d8b3a61039be4c9be418fb5b26f2a08c1be5f
Neale Upstone <neale@nealeupstone.com>
2025-12-09 22:58:21 +0000

    Set UUID represention that's now needed

--- src/main/java/org/example/mongoemptymaptest/MongoEmptyMapTestConfiguration.java
+++ src/main/java/org/example/mongoemptymaptest/MongoEmptyMapTestConfiguration.java
@@ -1,11 +1,25 @@
 package org.example.mongoemptymaptest;
 
+import com.mongodb.MongoClientSettings;
+import org.bson.UuidRepresentation;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @Configuration
 @ComponentScan
 @EnableMongoRepositories
-public class MongoEmptyMapTestConfiguration {
+public class MongoEmptyMapTestConfiguration extends AbstractMongoClientConfiguration {
+
+  @Override
+  protected String getDatabaseName() {
+    return "test";
+  }
+
+  @Override
+  protected void configureClientSettings(MongoClientSettings.Builder builder) {
+    builder.uuidRepresentation(UuidRepresentation.STANDARD);
+  }
+
 }
